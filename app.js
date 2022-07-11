08b79572e4703b0bfb7e7592ad1b84a70c2565ab
createit-jungjunghoon <92363576+walkerJung@users.noreply.github.com>
2022-07-11 22:46:19 +0900

    # 시퀄라이즈 실행 및 nodemon 설치

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const postRouter = require("./routes/post");
-
+const db = require("./models");
 const app = express();
+db.sequelize
+  .sync()
+  .then(() => {
+    console.log("db 연결 성공");
+  })
+  .catch(console.error);
 
 app.get("/", (req, res) => {
   res.send("Hello Express :)");
@@ -10,5 +16,5 @@ app.get("/", (req, res) => {
 app.use("/post", postRouter);
 
 app.listen(3065, () => {
-  console.log("server running!!!!!!!");
+  console.log("server running!!");
 });
--- models/post.js
+++ models/post.js
@@ -14,7 +14,7 @@ module.exports = (sequelize, DataTypes) => {
   );
   Post.associate = (db) => {
     db.Post.belongsTo(db.User);
-    db.Post.belongsToMany(db.Hashtag);
+    db.Post.belongsToMany(db.Hashtag, { through: "PostHashtag" });
     db.Post.hasMany(db.Comment);
     db.Post.hasMany(db.Image);
     db.Post.belongsToMany(db.User, { through: "Like", as: "Likers" });
