cb9ef537dc86278136eff83465fe3bc6f979d97b
createit-jungjunghoon <92363576+walkerJung@users.noreply.github.com>
2022-07-14 11:33:23 +0900

    # CORS

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const postRouter = require("./routes/post");
 const userRouter = require("./routes/user");
 const db = require("./models");
@@ -11,6 +12,12 @@ db.sequelize
   })
   .catch(console.error);
 
+app.use(
+  cors({
+    origin: "*",
+    credentials: false,
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
