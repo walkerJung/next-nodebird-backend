b29e5862af293feec371eafbd36bb0dc9bbab7cd
createit-jungjunghoon <92363576+walkerJung@users.noreply.github.com>
2022-07-14 18:12:27 +0900

    # 미들웨어 추가 및 로그아웃

--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
 const passport = require("passport");
-const { User } = require("../models");
+const { User, Post } = require("../models");
+const { isLoggedIn, isNotLoggedIn } = require("./middlewares");
 const router = express.Router();
 
-router.post("/login", (req, res, next) => {
+router.post("/login", isNotLoggedIn, (req, res, next) => {
   passport.authenticate("local", (err, user, info) => {
     if (err) {
       console.error(err);
@@ -18,7 +19,26 @@ router.post("/login", (req, res, next) => {
         console.error(loginErr);
         return next(loginErr);
       }
-      return res.json(user);
+      const fullUserWithoutPassword = await User.findOne({
+        where: { id: user.id },
+        attributes: {
+          exclude: ["password"],
+        },
+        include: [
+          {
+            model: Post,
+          },
+          {
+            model: User,
+            as: "Followings",
+          },
+          {
+            model: User,
+            as: "Followers",
+          },
+        ],
+      });
+      return res.json(fullUserWithoutPassword);
     });
   })(req, res, next);
 });
@@ -48,10 +68,10 @@ router.post("/", async (req, res, next) => {
   }
 });
 
-router.post("/user/logout", (req, res, next) => {
-  req.logout();
+router.post("/logout", isLoggedIn, (req, res) => {
+  req.logout(() => {});
   req.session.destroy();
-  res.send("ok");
+  return res.send("ok");
 });
 
 module.exports = router;
