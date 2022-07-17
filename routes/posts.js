11b8be0489041abca68c0612e53b9c7817ba4684
createit-jungjunghoon <92363576+walkerJung@users.noreply.github.com>
2022-07-17 22:56:23 +0900

    # lastId + infinityScroll

--- routes/posts.js
+++ routes/posts.js
@@ -1,11 +1,16 @@
 const express = require("express");
+const { Op } = require("sequelize");
 const { Post, User, Image, Comment } = require("../models");
 const router = express.Router();
 
 router.get("/", async (req, res, next) => {
   try {
+    const where = {};
+    if (parseInt(req.query.lastId, 10)) {
+      where.id = { [Op.lt]: parseInt(req.query.lastId, 10) };
+    }
     const posts = await Post.findAll({
-      //   where: { id: lastId },
+      where,
       limit: 10,
       order: [
         ["createdAt", "DESC"],
